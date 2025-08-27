98b34801eedeb228fea115693579106e8730a316
Cheri Triggers <cheritriggers@gmail.com>
2025-08-27 13:23:43 -0500

    Created a UniversalAudioReader to handle multiple file types.

--- Triggerless.TriggerBot/Components/UniversalAudioReader.cs
+++ Triggerless.TriggerBot/Components/UniversalAudioReader.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using NAudio.Wave;
+using NAudio.Vorbis; // OGG via NVorbis
 
-namespace Triggerless.TriggerBot
+public static class UniversalAudioReader
 {
-    internal class UniversalAudioReader
+    public static WaveStream Open(string filePath)
     {
+        var ext = Path.GetExtension(filePath).ToLowerInvariant();
+
+        switch (ext)
+        {
+            case ".wav":
+                return new WaveFileReader(filePath);
+
+            case ".ogg":
+                return new VorbisWaveReader(filePath); // requires NAudio.Vorbis
+
+            case ".mp3":
+                // MediaFoundationReader handles mp3 too; fall back to Mp3FileReader if MF not available
+                try { return new MediaFoundationReader(filePath); }
+                catch { return new Mp3FileReader(filePath); }
+
+            case ".flac":
+                // Use Media Foundation FLAC decoder (Win10/11). Throws if codec not present.
+                return new MediaFoundationReader(filePath);
+
+            default:
+                // Best-effort: let Media Foundation try (aac, wma, m4a, etc.)
+                return new MediaFoundationReader(filePath);
+        }
     }
 }
